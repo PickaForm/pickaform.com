49f78a9482d1ed36c18334d55e10d3f86288a0e8
DavidGrossi <david.grossi@pickaform.com>
2023-06-05 23:41:14 +0400

    Navbar test

--- views/navbar.js
+++ views/navbar.js
@@ -197,7 +197,7 @@ kiss.app.defineView("navbar", function (id, target) {
 
         methods: {
             load() {
-                this.adjustDisplayMode(kiss.screen.current.width)
+                // this.adjustDisplayMode(kiss.screen.current.width)
             },
             _afterConnected() {
                 this.translateTo(kiss.language.current)
