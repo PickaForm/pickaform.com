8731070fe907680eeaade6d7d7820e401c40557a
DavidGrossi <david.grossi@pickaform.com>
2023-06-14 18:59:52 +0400

    Improved Example page

--- views/cases.js
+++ views/cases.js
@@ -52,38 +52,70 @@ kiss.app.defineView("cases", function (id, target) {
             en: "Manage the life cycle of your contracts, monitor their deadlines, their renewals, and follow in a workflow the evolutions to be brought in a collaborative way.",
             fr: "Gérez le cycle de vie de vos contrats, surveillez leurs échéances, leurs renouvellements, et suivez dans un workflow les évolutions à apporter de manière collaborative."
         },
+        titlePhone: {
+            en: "Phone call tracking",
+            fr: "Suivi des appels téléphoniques"
+        },
+        descriptionPhone: {
+            en: "Does your company have to deal with customer requests over the phone? Not a problem, many of our clients already track their calls and their handling status via custom workflows.",
+            fr: "Votre société doit traiter des demandes clients par téléphone ? Pas problème, plusieurs de nos clients suivent déjà leurs appels et leur état de traitement via des workflows sur mesure."
+        },        
+        titleRequests: {
+            en: "Follow-up of requests",
+            fr: "Suivi des demandes"
+        },
+        descriptionRequests: {
+            en: "Purchasing requests, intervention requests, authorization requests, leave requests, requests for... whatever you want! Track all your internal requests through one simple tool.",
+            fr: "Demandes d'achats, demandes d'interventions, demandes d'habilitation, demandes de congés, demandes de... ce que vous voulez ! Suivez toutes vos demandes internes via un seul outil simple."
+        },        
         titleSupport: {
             en: "Support, ticketing system",
             fr: "Support, gestion de tickets"
         },
         descriptionSupport: {
-            en: "",
-            fr: ""
-        },
-        titleProduct: {
-            en: "Product design",
-            fr: "Conception de produit"
-        },
-        descriptionProduct: {
-            en: "",
-            fr: ""
-        },
-        titleInventory: {
-            en: "Inventory management",
-            fr: "Gestion de l'inventaire"
-        },
-        descriptionInventory: {
-            en: "",
-            fr: ""
+            en: "There are many support and ticket management applications on the market, but none will allow you as much flexibility as a custom design for your business.",
+            fr: "Les applications de support et gestion de tickets sont nombreuses sur le marché, mais aucune ne vous permettra autant de flexibilité qu'une conception sur mesure pour votre métier."
         },
         titleRecovery: {
             en: "amicable negotiation, litigation, recovery",
             fr: "négociation amiable, contentieux, recouvrement"
         },
         descriptionRecovery: {
-            en: "",
-            fr: ""
+            en: "Some of our clients use pickaform to manage their amicable negotiation, litigation and recovery workflows in order to have a cross-sectional view of all the files in progress.",
+            fr: "Certains de nos clients utilisent pickaform pour gérer leurs workflows de négociation amiable, contentieux, et recouvrement afin d'avoir une vision transversale de tous les dossiers en cours."
         },
+        titleProduct: {
+            en: "Product design",
+            fr: "Conception de produit"
+        },
+        descriptionProduct: {
+            en: "If you design products, you have to collaborate with your teams on many different aspects, from design to marketing. Pickaform allows you to federate and share all the information you need.",
+            fr: "Si vous concevez des produits, vous devez collaborer avec vos équipes sur plein d'aspects différents, du design jusqu'au marketing. Pickaform vous permet de féréder et partager toutes les informations dont vous avez besoin."
+        },
+        titleInventory: {
+            en: "Inventory management",
+            fr: "Gestion de l'inventaire"
+        },
+        descriptionInventory: {
+            en: "Whether you manage a computer park, construction equipment, or something else, pickaform allows you to build a simple and tailor-made solution to track all your equipment.",
+            fr: "Que vous gériez un parc informatique, du matériel de chantier, ou autre chose, pickaform permet de construire une solution simple et sur mesure pour traquer tous vos matériels."
+        },
+        titleDonation: {
+            en: "Donation management",
+            fr: "Gestion des dons"
+        },
+        descriptionDonation: {
+            en: "You are a large association and you need to organize yourself to monitor the donations collected and their allocation? We got you covered!",
+            fr: "Vous êtes une association de taille importante et vous avez besoin de vous organiser pour suivre les dons collectés et leur attribution ? Nous vous couvrons !"
+        },
+        titleAnything: {
+            en: "Management of...",
+            fr: "Gestion de..."
+        },
+        descriptionAnything: {
+            en: "The examples above are given as an example, but you manage exactly what you want with pickaform, and the only limit is your imagination!",
+            fr: "Les exemples ci-dessus sont donnés à titre d'exemple, mais vous gérez exactement ce que vous voulez avec pickaform, et la seule limite est votre imagination !"
+        },        
         titleBookDemo: {
             en: `Not convinced?`,
             fr: `Pas convaincu ?`
@@ -399,6 +431,30 @@ kiss.app.defineView("cases", function (id, target) {
                 backgroundColor: "var(--feature-background)"
             }),
 
+            // PHONE
+            kiss.templates.feature({
+                title: t("titlePhone"),
+                description: t("descriptionPhone"),
+                screenshot: "workflows-thumbnail.webp",
+                CTA: t("getStarted"),
+                textPosition: "left"
+            }),
+
+            {
+                class: "feature-top-separator"
+            },
+
+            // REQUESTS
+            kiss.templates.feature({
+                title: t("titleRequests"),
+                description: t("descriptionRequests"),
+                screenshot: "integrated simple and powerful form designer - pickaform.webp",
+                CTA: t("getStarted"),
+                textPosition: "right",
+                color: "#555555",
+                backgroundColor: "var(--feature-background)"
+            }),            
+
             // SUPPORT
             kiss.templates.feature({
                 title: t("titleSupport"),
@@ -412,11 +468,11 @@ kiss.app.defineView("cases", function (id, target) {
                 class: "feature-top-separator"
             },
 
-            // PRODUCT
+            // RECOVERY
             kiss.templates.feature({
-                title: t("titleProduct"),
-                description: t("descriptionProduct"),
-                screenshot: "views-thumbnail.webp",
+                title: t("titleRecovery"),
+                description: t("descriptionRecovery"),
+                screenshot: "navigate through your data with tabs - pickaform.webp",
                 CTA: t("getStarted"),
                 textPosition: "right",
                 color: "#555555",
@@ -436,16 +492,40 @@ kiss.app.defineView("cases", function (id, target) {
                 class: "feature-top-separator"
             },
 
-            // RECOVERY
+            // PRODUCT
             kiss.templates.feature({
-                title: t("titleRecovery"),
-                description: t("descriptionRecovery"),
-                screenshot: "navigate through your data with tabs - pickaform.webp",
+                title: t("titleProduct"),
+                description: t("descriptionProduct"),
+                screenshot: "views-thumbnail.webp",
                 CTA: t("getStarted"),
                 textPosition: "right",
                 color: "#555555",
                 backgroundColor: "var(--feature-background)"
             }),
+
+            // DONATION
+            kiss.templates.feature({
+                title: t("titleDonation"),
+                description: t("descriptionDonation"),
+                screenshot: "navigate through your data with left pane - pickaform.webp",
+                CTA: t("getStarted"),
+                textPosition: "left"
+            }),
+
+            {
+                class: "feature-top-separator"
+            },
+
+            // ANYTHING
+            kiss.templates.feature({
+                title: t("titleAnything"),
+                description: t("descriptionAnything"),
+                screenshot: "views-thumbnail.webp",
+                CTA: t("getStarted"),
+                textPosition: "right",
+                color: "#555555",
+                backgroundColor: "var(--feature-background)"
+            }),            
 
             // BOOK A DEMO
             kiss.templates.title({
