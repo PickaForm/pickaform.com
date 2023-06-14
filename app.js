55ce4b8d0eb7d9e545bc9438ed5d590d76bb27f3
DavidGrossi <david.grossi@pickaform.com>
2023-06-15 02:36:41 +0400

    Fixed pricing page

--- app.js
+++ app.js
@@ -1794,19 +1794,38 @@ function translate() {
             en: "Contact us",
             fr: "Contactez-nous"
         },
+        faq0: {
+            en: "Why choose pickaform?",
+            fr: "Pourquoi choisir pickaform ?"
+        },
+        answer0: {
+            en: `Pickaform is not a cloud software born from a simple idea and created quickly to surf the wave of no-code.
+            The story begins in 2009, long before "no-code" existed, and the design of the platform was 100% guided by the use of its users.
+            In 2019, ten years after the very first version, the platform is dedicated "IBM CHAMPION" in the category of Collaboration software.
+            From the outset, the platform has kept the same philosophy:
+            <br>- easy-to-make forms
+            <br>- a la carte functionalities integrated into forms
+            <br>- flexible and quick-to-implement workflow feature
+            <br>- simplicity
+            <br>Today, large companies use Pickaform on a daily basis for all types of use, and 95% of our users who adopt the platform stay on it for several years, guaranteeing maximum ROI!`,
+            fr: `Pickaform n'est pas un logiciel cloud issu d'une simple idée et créé rapidement pour surfer sur la vague du no-code.
+            L'histoire commence en 2009, bien avant que le "no-code" existe, et la conception de la plateforme a été guidée à 100% par l'usage de ses utilisateurs.
+            En 2019, soit dix ans après la toute première version, la plateforme est consacrée "IBM CHAMPION" dans la categorie des logiciels de Collaboration.
+            Depuis l'origine, la plateforme a conservé la même philosophie :
+            <br>- formulaires simples à fabriquer
+            <br>- fonctionnalités à la carte intégrées aux formulaires
+            <br>- fonctionnalité de workflow flexible et rapide à mettre en place
+            <br>- simplicité
+            <br>Aujourd'hui, de grandes entreprises utilisent Pickaform au quotidien pour tous types d'usages, et 95% de nos utilisateurs qui adoptent la plateforme restent dessus pour plusieurs années, garantissant un ROI maximal !`
+        },        
         faq1: {
-            en: "Who uses PickaForm?",
-            fr: "A qui s'adresse PickaForm ?"
+            en: "Who uses Pickaform?",
+            fr: "A qui s'adresse Pickaform ?"
         },
         answer1: {
-            en: `PickaForm is great for:
-            - enterprises
-            - associations
-            - public services
-            - freelancers
-            - anyone who wants to organize structured data
-            More generally, anyone who wants to organize data, to access it online securely, and eventually to share it with some teammates.`,
-            fr: `PickaForm est fait pour : les entreprises, les associations, le secteur public, les travailleurs indépendants, les particuliers qui veulent organiser des informations.
+            en: `Pickaform is made for: companies, associations, the public sector, the self-employed, individuals who want to organize information.
+            More generally, to any person or organization wishing to organize their data and be able to access it online in a secure and possibly shared way with other users.`,
+            fr: `Pickaform est fait pour : les entreprises, les associations, le secteur public, les travailleurs indépendants, les particuliers qui veulent organiser des informations.
             Plus généralement, à toute personne ou organisation souhaitant organiser ses données et pouvoir y accéder en ligne de manière sécurisée et éventuellement partagée avec d'autres utilisateurs.`
         },
         faq2: {
@@ -1815,9 +1834,9 @@ function translate() {
         },
         answer2: {
             en: `First, sign up and enjoy your free trial including up to 20 users!
-            If you need to upgrade before the end of your free period, you'll do it directly from your PickaForm account management.`,
+            If you need to upgrade before the end of your free period, you'll do it directly from your Pickaform account management.`,
             fr: `Inscrivez-vous et profitez d'abord de votre période d'essai, incluant jusqu'à 20 utilisateurs !
-            Si vous souhaitez changer d'offre avant la fin de votre essai, vous pourrez le faire directement dans PickaForm depuis votre rubrique "Compte".`
+            Si vous souhaitez changer d'offre avant la fin de votre essai, vous pourrez le faire directement dans Pickaform depuis votre rubrique "Compte".`
         },
         faq3: {
             en: "When will I be able to stop my subscription?",
@@ -1876,13 +1895,13 @@ function translate() {
             En clair, nous sommes imbattables sur le prix lorsque l'on est comparé à des produits de fonctionnalités similaires.`
         },
         faq8: {
-            en: "How much data can I store on PickaForm?",
+            en: "How much data can I store on Pickaform?",
             fr: "A quel volume de données ai-je le droit ?"
         },
         answer8: {
-            en: `PickaForm allows you to attach files to your form data, but its goal is not to replace a file management or a virtual drive (like Google Drive, One Drive, DropBox ...), so, we calculated that an average volume of 100MB of attachments per user was sufficient for standard use of our platform. For example, this represents 2GB of data for 20 users.
+            en: `Pickaform allows you to attach files to your form data, but its goal is not to replace a file management or a virtual drive (like Google Drive, One Drive, DropBox ...), so, we calculated that an average volume of 100MB of attachments per user was sufficient for standard use of our platform. For example, this represents 2GB of data for 20 users.
             Contact us if you think your needs will exceed this usage. In that case, there is an option to connect your own storage.`,
-            fr: `PickaForm vous permet bien de joindre des fichiers à vos données de formulaire, mais sa fonction n'est pas de venir en remplacement d'une gestion de fichiers ou d'un drive (Google Drive, One Drive, DropBox...), aussi, nous avons calculé qu'un volume moyen de 100Mo de pièces jointes par utilisateur était suffisant pour un usage standard de notre plateforme. Cela représente 2Go de données pour 20 utilisateurs.
+            fr: `Pickaform vous permet bien de joindre des fichiers à vos données de formulaire, mais sa fonction n'est pas de venir en remplacement d'une gestion de fichiers ou d'un drive (Google Drive, One Drive, DropBox...), aussi, nous avons calculé qu'un volume moyen de 100Mo de pièces jointes par utilisateur était suffisant pour un usage standard de notre plateforme. Cela représente 2Go de données pour 20 utilisateurs.
             Nous consulter si vous pensez que vos besoins dépasseront cet usage. Si c'est le cas, il existe une possibilité pour intégrer votre propre espace de stockage.`
         }
     })
@@ -1925,30 +1944,35 @@ function translate() {
                 title: t("title"),
                 subtitle: t("subtitle"),
             }),
+
+            // PRICING TABLE
             {
                 type: "html",
                 class: "pricing-table",
                 html: kiss.templates.pricingTable(plans, t)
             },
+
+            // ENTERPRISE PLAN
             {
                 type: "html",
                 class: "pricing-enterprise",
                 html: t("enterprisePlan")
             },
+
+            // CONTACT
             kiss.templates.buttonCTA(t("contactUs"), "contact"),
-            {
-                height: 100
-            },
+
+            // FAQ
             {
                 class: "feature-top-separator"
             },
-            // FAQ
             {
                 class: "pricing-faq-container",
                 items: [
                     {
                         class: "pricing-faq-container-column",
                         items: [
+                            kiss.templates.pricingFAQ(t("faq0"), t("answer0")),
                             kiss.templates.pricingFAQ(t("faq1"), t("answer1")),
                             kiss.templates.pricingFAQ(t("faq2"), t("answer2")),
                             kiss.templates.pricingFAQ(t("faq3"), t("answer3")),
