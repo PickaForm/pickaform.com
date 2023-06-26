379d1a261965414ce8084b2d6fd255dee92dd007
DavidGrossi <david.grossi@pickaform.com>
2023-06-26 20:09:41 +0400

    Updated links from cloud.pickaform to app.pickaform

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ kiss.theme.set({
 // Paths
 kiss.global.path = `https://${window.location.host}`
 kiss.global.pathImg = "./resources/img"
-kiss.global.pathPickaform = `https://cloud.pickaform.com`
+kiss.global.pathPickaform = `https://app.pickaform.com`
 
 // Blog
-kiss.global.blogEndPoint = "https://cloud.pickaform.com/command/blog"
+kiss.global.blogEndPoint = "https://app.pickaform.com/command/blog"
 kiss.global.blogModelId = "0187ed51-d3a5-70ea-869c-6c538d786fb7"
 kiss.global.blogPostTitle = "y9yVRPEQ"
 kiss.global.blogPostDescription = "BedquzD8"
@@ -24,7 +24,7 @@ kiss.global.blogPostPublished = "rhI4E1iH"
 kiss.global.contactModelId = "0187b40b-0061-7f65-af86-982a361afcf3"
 
 // AI Art
-kiss.global.artEndPoint = "https://cloud.pickaform.com/command/product"
+kiss.global.artEndPoint = "https://app.pickaform.com/command/product"
 kiss.global.artModelId = "01889cf0-5878-7352-93b5-3a0fb88c852f"
 kiss.global.artTitle = "pJZ5QvWL"
 kiss.global.artPulished = "DgllE0KD";/**
