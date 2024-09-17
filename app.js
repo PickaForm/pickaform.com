d4d7b63dedaa2a1ad13bc5e20282ff385c44f43b
David Grossi <david.grossi@pickaform.com>
2024-09-17 18:18:09 +0400

    Added comparison strip on home page

--- app.js
+++ app.js
@@ -1453,7 +1453,174 @@ function translate() {
                         <div class="a-button button-booking">Réservez votre démo privée dès maintenant pour bénéficier de cette offre unique !</div>
                     </a>
                 </center>`
-            }
+            },
+            titleWhy: {
+                en: `Why Pickaform?`,
+                fr: `Pourquoi Pickaform ?`
+            },
+            subtitleWhy: {
+                en: `Simply best suited for
+                    <br><span class="text-highlight" style="background-color: #00aaee">enterprises</span>`,
+                fr: `Simplement la plus adaptée aux
+                    <br><span class="text-highlight" style="background-color: #00aaee">entreprises</span>`
+            },
+            compareTo: {
+                en: `Compare to`,
+                fr: `Comparer à`
+            },
+            pickaform: {
+                en: `
+                    <br>✔ Rich form layout
+                    <br>✔ Integrated workflow engine (BPM)
+                    <br>✔ Integrated PDF generation
+                    <br>✔ Very fast and stable
+                    <br>✔ Cloud + self-hosted deployments
+                    <br>✔ Hosted in France
+                    <br>✔ GDPR
+                    <br>✔ Competitive pricing
+                    <br>✔ English, French, Spanish & more coming
+                    <br>✅ Good for all company sizes and budgets
+                    `,
+                fr: `
+                    <br>✔ Mise en page de formulaire riche
+                    <br>✔ Moteur de workflow intégré (BPM)
+                    <br>✔ Génération de PDF intégrée
+                    <br>✔ Très rapide et très stable
+                    <br>✔ Validé par de grandes entreprises
+                    <br>✔ Déploiements cloud et on-premise
+                    <br>✔ Hébergé en France
+                    <br>✔ RGPD
+                    <br>✔ Tarification compétitive
+                    <br>✔ Anglais, français, espagnol et plus à venir
+                    <br>✅ Bon pour toutes les tailles d'entreprises et de budgets
+                `,
+
+            },
+            airtable: {
+                en: `
+                    <br>❌ Cloud only
+                    <br>❌ Poor security model
+                    <br>❌ Basic form layout, no sections, no columns
+                    <br>❌ No workflow engine (BPM)
+                    <br>❌ No native PDF generation
+                    <br>❌ Low row limits, even at the enterprise level
+                    <br>❌ Lacks speed and stability at scale
+                    <br>❌ Complex pricing
+                    <br>❌ Expensive for large teams
+                    <br>❌ English only
+                    <br>✅ Good for companies with a substantial budget
+                    `,
+                fr: `
+                    <br>❌ Cloud uniquement
+                    <br>❌ Modèle de sécurité médiocre
+                    <br>❌ Mise en page basique des formulaire
+                    <br>❌ Pas de sections, pas de colonnes
+                    <br>❌ Pas de moteur de workflow (BPM)
+                    <br>❌ Pas de génération native de PDF
+                    <br>❌ Limitations sur la quantité de données
+                    <br>❌ Manque de vitesse et de stabilité à grande échelle
+                    <br>❌ Tarification complexe
+                    <br>❌ Cher pour les grandes équipes
+                    <br>❌ Anglais seulement
+                    <br>✅ Bon pour les entreprises avec un budget conséquent
+                `,
+            },
+            baserow: {
+                en: `
+                    <br>❌ Airtable clone with inferior UI/UX
+                    <br>❌ Poor security model
+                    <br>❌ Basic form layout, no sections, no columns
+                    <br>❌ No workflow engine (BPM)
+                    <br>❌ No native PDF generation
+                    <br>❌ Not so intuitive for non-technical users
+                    <br>❌ Difficult data aggregations
+                    <br>❌ English only
+                    <br>✅ Good for basic projects and small teams
+                    `,
+                fr: `
+                    <br>❌ Clone de Airtable avec une UI/UX inférieure
+                    <br>❌ Modèle de sécurité médiocre
+                    <br>❌ Mise en page basique des formulaire
+                    <br>❌ Pas de sections, pas de colonnes
+                    <br>❌ Pas de moteur de workflow (BPM)
+                    <br>❌ Pas de génération native de PDF
+                    <br>❌ Pas intuitif pour les utilisateurs non techniques
+                    <br>❌ Agrégations de données difficiles
+                    <br>❌ Anglais seulement
+                    <br>✅ Bon pour les projets basiques et les petites équipes
+                `,
+            },
+            nocodb: {
+                en: `
+                    <br>❌ Airtable clone with inferior UI/UX
+                    <br>❌ Very slow UI when navigating datasets
+                    <br>❌ Basic form layout, no sections, no columns
+                    <br>❌ No workflow engine (BPM)
+                    <br>❌ No native PDF generation
+                    <br>❌ Designed for technical audiences
+                    <br>❌ Lacks collaborative features for teams
+                    <br>❌ English only
+                    <br>✅ Good for technical teams
+                    `,
+                fr: `
+                    <br>❌ Clone de Airtable avec une UI/UX inférieure
+                    <br>❌ UI lente lors de la navigation dans les données
+                    <br>❌ Mise en page basique des formulaire
+                    <br>❌ Pas de sections, pas de colonnes
+                    <br>❌ Pas de moteur de workflow (BPM)
+                    <br>❌ Pas de génération native de PDF
+                    <br>❌ Conçu pour les audiences techniques
+                    <br>❌ Manque de fonctionnalités collaboratives
+                    <br>❌ Anglais seulement
+                    <br>✅ Bon pour les équipes techniques
+                `,
+            },            
+            googlesheets: {
+                en: `
+                    <br>❌ Cloud only
+                    <br>❌ Not suited to build enterprise apps
+                    <br>❌ Impossible to fine-tune security
+                    <br>❌ No forms at all
+                    <br>❌ No relational/linking abilities
+                    <br>❌ No workflow engine (BPM)
+                    <br>❌ No native PDF generation
+                    <br>❌ Lacks collaborative features for teams
+                    <br>❌ Long history of privacy and security concerns
+                    <br>✅ Great for spreadsheet work and calculations
+                    `,
+                fr: `
+                    <br>❌ Cloud uniquement
+                    <br>❌ Pas adapté pour des applications d'entreprise
+                    <br>❌ Impossible d'avoir une sécurité fine
+                    <br>❌ Pas de formulaires du tout
+                    <br>❌ Aucune capacité de relation / liaison
+                    <br>❌ Pas de moteur de workflow (BPM)
+                    <br>❌ Pas de génération native de PDF
+                    <br>❌ Manque de fonctionnalités collaboratives
+                    <br>❌ Préoccupations en matière de confidentialité
+                    <br>✅ Idéal comme tableur et pour les calculs
+                `,
+            },
+            excelsheets: {
+                en: `
+                    <br>❌ Not suited to build enterprise apps
+                    <br>❌ Impossible to fine-tune security
+                    <br>❌ No relational/linking abilities
+                    <br>❌ No workflow engine (BPM)
+                    <br>❌ No native PDF generation
+                    <br>❌ Lacks collaborative features for teams
+                    <br>✅ Great for spreadsheet work and calculations
+                    `,
+                fr: `
+                    <br>❌ Pas adapté pour des applications d'entreprise
+                    <br>❌ Impossible d'avoir une sécurité fine
+                    <br>❌ Pas de capacité de relation / liaison
+                    <br>❌ Pas de moteur de workflow (BPM)
+                    <br>❌ Pas de génération native de PDF
+                    <br>❌ Manque de fonctionnalités collaboratives
+                    <br>✅ Idéal comme tableur et pour les calculs
+                `,
+            }           
         })
 
         return createBlock({
@@ -1493,12 +1660,97 @@ function translate() {
                 kiss.templates.buttonCTA(t("bookDemo"), "booking"),
                 kiss.templates.screenshot("midjourney integration - pickaform.webp"),
 
-                // STRIP 4
+                // STRIP 4 - Comparison with competitors
+                kiss.templates.title({
+                    title: t("titleWhy"),
+                    subtitle: t("subtitleWhy")
+                }),
+
+                {
+                    class: "vs-container",
+                    items: [
+                        {
+                            layout: "vertical",
+                            class: "vs-content",
+                            items: [
+                                {
+                                    type: "html",
+                                    html: `<img src="./resources/img/pickaform.webp" alt="pickaform logo" class="image-content" loading="lazy" style="width: 256px; height: 60px;">`
+                                },
+                                {
+                                    type: "html",
+                                    html: t("pickaform")
+                                }
+                            ]
+                        },
+                        {
+                            type: "html",
+                            html: "<div class='vs'>VS</div>"
+                        },
+                        {
+                            layout: "vertical",
+                            class: "vs-content",
+                            items: [
+                                {
+                                    type: "select",
+                                    class: "vs-select",
+                                    label: t("compareTo"),
+                                    labelPosition: "top",
+                                    fieldWidth: "100%",
+                                    labelWidth: "100%",
+                                    value: "airtable",
+                                    autocomplete: "off",
+                                    options: [
+                                        {
+                                            label: "Airtable",
+                                            value: "airtable",
+                                            color: "#39CAFF"
+                                        },
+                                        {
+                                            label: "Nocodb",
+                                            value: "nocodb",
+                                            color: "#322EBA"
+                                        },
+                                        {
+                                            label: "Baserow",
+                                            value: "baserow",
+                                            color: "#5186D7"
+                                        },
+                                        {
+                                            label: "Google Sheets",
+                                            value: "googlesheets",
+                                            color: "#34A853"
+                                        },
+                                        {
+                                            label: "Microsoft Excel",
+                                            value: "excelsheets",
+                                            color: "#107C41"
+                                        }
+                                    ],
+                                    events: {
+                                        change: function() {
+                                            const value = this.getValue()
+                                            const targetProduct = document.getElementById("target-product")
+                                            targetProduct.innerHTML = t(value)
+                                        }
+                                    }
+                                },                        
+                                {
+                                    id: "target-product",
+                                    type: "html",
+                                    html: t("airtable")
+                                }                                
+                            ]
+                        }
+                    ]
+                },
+
+                // STRIP 5
                 kiss.templates.title({
                     title: t("titleExperience"),
                     subtitle: t("subtitleExperience")
                 }),
-                kiss.templates.buttonCTA(t("bookDemo"), "booking"),
+                kiss.templates.buttonCTA(t("bookDemo"), "booking"),                
 
                 // CUSTOMERS
                 kiss.templates.screenshot("customer references - pickaform.webp"),
